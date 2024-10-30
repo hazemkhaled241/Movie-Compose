facaf020e5ab5e72a368edb89458ac42265263c2
Mr.Hazem <hazemkhaled3166589@gmail.com>
2024-10-30 14:43:51 +0300

    fix some bugs

--- app/src/main/java/com/hazem/movie_app/presentation/screen/home/component/MovieItem.kt
+++ app/src/main/java/com/hazem/movie_app/presentation/screen/home/component/MovieItem.kt
@@ -8,13 +8,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -40,31 +39,28 @@ import com.hazem.movie_app.presentation.navigation.Screen
 
 @Composable
 fun MovieItem(navController: NavHostController, movie: Movie) {
-
     Card(
-        modifier = Modifier
+        Modifier
             .padding(10.dp)
             .clickable {
                 navController.navigate(Screen.DetailsScreen(movie.id))
             },
         elevation = CardDefaults.cardElevation(8.dp)
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier, contentAlignment = Alignment.BottomCenter) {
             AsyncImage(
                 model = movie.poster,
-                contentDescription = stringResource(R.string.movie_image),
+                contentDescription = movie.title,
                 modifier = Modifier
-                    .fillMaxSize()
+                    .height(250.dp)
                     .clip(RoundedCornerShape(10.dp)),
                 contentScale = ContentScale.Crop
             )
-
             Column(
                 modifier = Modifier
                     .fillMaxHeight()
-                    .background(Color.LightGray.copy(0.7f))
+                    .background(Color.LightGray.copy(.7f))
                     .padding(6.dp)
-                    .align(Alignment.BottomCenter)
             ) {
                 Text(
                     text = movie.title,
@@ -77,31 +73,23 @@ fun MovieItem(navController: NavHostController, movie: Movie) {
                     maxLines = 1,
                     style = TextStyle(
                         shadow = Shadow(
-                            color = Color(0xFFFC5A03),
-                            offset = Offset(1f, 1f),
-                            blurRadius = 3f
+                            Color(0xFFFC6603), offset = Offset(1f, 1f), 3f
                         )
                     )
                 )
                 Spacer(modifier = Modifier.height(8.dp))
-
-                Row(modifier = Modifier.align(Alignment.End)) {
-                    Icon(
-                        imageVector = Icons.Default.Star,
-                        contentDescription = stringResource(R.string.star_icon)
-                    )
-
+                Row(Modifier.align(Alignment.End)) {
+                    Icon(imageVector = Icons.Rounded.Star, contentDescription = stringResource(id = R.string.star_icon))
                     Text(
                         text = movie.imdbRating,
+                        textAlign = TextAlign.Start,
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(8.dp),
-                        textAlign = TextAlign.Start,
-                        color = Color.Black,
+                            .padding(start = 8.dp),
                         fontWeight = FontWeight.Bold,
-                        maxLines = 2,
-
-                        )
+                        color = Color.Black,
+                        maxLines = 2
+                    )
                 }
             }
         }
